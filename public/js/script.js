be6110e85005414ff1eace4fe14c2e67493445ab
eni <eni@shipbuilders.ru>
2024-05-09 23:50:13 +0300

    Добавлена мобильная навигация

--- public/js/script.js
+++ public/js/script.js
@@ -12,60 +12,10 @@ function checkNumValue(input) {
     }
 }
 
+const burgerButton = document.querySelector('.js-header__burger');
+const navMenu = document.querySelector('.mobile-container__wrapper');
 
-const rangeInput = document.querySelectorAll(".wrapper-double-filter__range-input input"),
-priceInput = document.querySelectorAll(".wrapper-double-filter__price-input input"),
-range = document.querySelector(".wrapper-double-filter__slider .progress");
-let priceGap = 500;
-
-priceInput.forEach(input =>{
-    input.addEventListener("input", e =>{
-        let minPrice = parseInt(priceInput[0].value),
-        maxPrice = parseInt(priceInput[1].value);
-        
-        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
-            if(e.target.className === "input-min"){
-                rangeInput[0].value = minPrice;
-                range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
-            }else{
-                rangeInput[1].value = maxPrice;
-                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-            }
-        }
-    });
-});
-
-priceInput[0].addEventListener("input", function(e) {
-    let minPrice = parseInt(this.value);
-    let maxPrice = parseInt(priceInput[1].value);
-    if (minPrice >= maxPrice) {
-        this.value = maxPrice - priceGap;
-    }
-});
-
-priceInput[1].addEventListener("input", function(e) {
-    let minPrice = parseInt(priceInput[0].value);
-    let maxPrice = parseInt(this.value);
-    if (maxPrice <= minPrice) {
-        this.value = minPrice + priceGap;
-    }
-});
-
-rangeInput.forEach(input =>{
-    input.addEventListener("input", e =>{
-        let minVal = parseInt(rangeInput[0].value),
-        maxVal = parseInt(rangeInput[1].value);
-        if((maxVal - minVal) < priceGap){
-            if(e.target.className === "range-min"){
-                rangeInput[0].value = maxVal - priceGap
-            }else{
-                rangeInput[1].value = minVal + priceGap;
-            }
-        }else{
-            priceInput[0].value = minVal;
-            priceInput[1].value = maxVal;
-            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
-            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-        }
-    });
+burgerButton.addEventListener('click', function() {
+    navMenu.classList.toggle('mobile-container__wrapper-on');
+    this.classList.toggle('js-header__burger_opened');
 });
